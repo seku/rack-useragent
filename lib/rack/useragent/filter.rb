988b056bb9059424f137b049e0ae69d6ea5f6732
Michael Guterl <mguterl@gmail.com>
2010-10-08 13:08:18 -0400

    it doesn't appear that IE renders the content of a response with a 400 status

--- lib/rack/useragent/filter.rb
+++ lib/rack/useragent/filter.rb
@@ -6,7 +6,7 @@ require 'ostruct'
 module Rack
   module UserAgent
     class Filter
-      
+
       def initialize(app, required_browsers = [], options = {})
         @app                = app
         @browsers           = required_browsers.map{ |b| OpenStruct.new(:browser => b[0], :version => b[1]) }
@@ -19,7 +19,7 @@ module Rack
         useragent = ::UserAgent.parse(env["HTTP_USER_AGENT"].to_s)
         if useragent.any? && !detection_disabled_by_cookie?(request.cookies) && unsupported?(useragent)
           content = render_page(useragent)
-          [400, {"Content-Type" => "text/html", "Content-Length" => content.length.to_s}, content]
+          [200, {"Content-Type" => "text/html", "Content-Length" => content.length.to_s}, content]
         else
           @app.call(env)
         end
@@ -40,7 +40,7 @@ module Rack
           @browser = useragent # for the template
           Tilt.new(@template).render(self)
         else
-          "Sorry, your browser is not supported. Please upgrade" 
+          "Sorry, your browser is not supported. Please upgrade"
         end
       end
     end
